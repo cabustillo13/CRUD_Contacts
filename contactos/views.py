a422e62739eb5df62e8aecf6ad14c7dde3144c41
cabustillo13 <cabustillo13@hotmail.com>
2021-04-14 23:23:53 -0300

    [Fix] Views file

--- contactos/views.py
+++ contactos/views.py
@@ -3,20 +3,46 @@
 """Este módulo proporciona vistas para administrar la tabla Contactos."""
 
 from PyQt5.QtWidgets import (
+    QAbstractItemView,
     QHBoxLayout,
     QMainWindow,
+    QPushButton,
+    QTableView,
+    QVBoxLayout,
     QWidget,
 )
+
 
 class Window(QMainWindow):
     """Ventana principal."""
 
     def __init__(self, parent=None):
-        #"""Inicializador."""
+        """Inicializador."""
         super().__init__(parent)
         self.setWindowTitle("RP Contacts")
         self.resize(550, 250)
         self.centralWidget = QWidget()
         self.setCentralWidget(self.centralWidget)
         self.layout = QHBoxLayout()
-        self.centralWidget.setLayout(self.layout) 
+        self.centralWidget.setLayout(self.layout)
+
+        self.setupUI()
+
+    def setupUI(self):
+        """Setup la GUI de la ventana principal."""
+        # Crear la table view widget
+        self.table = QTableView()
+        self.table.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.table.resizeColumnsToContents()
+        # Create buttons
+        self.addButton = QPushButton("Add...")
+        self.deleteButton = QPushButton("Delete")
+        self.clearAllButton = QPushButton("Clear All")
+        # Extender la GUI
+        layout = QVBoxLayout()
+        layout.addWidget(self.addButton)
+        layout.addWidget(self.deleteButton)
+        layout.addStretch()
+        layout.addWidget(self.clearAllButton)
+        self.layout.addWidget(self.table)
+        self.layout.addLayout(layout)
